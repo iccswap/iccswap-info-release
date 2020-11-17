49996069b70afff250189060440fb1b7c2546d18
xiaojie <xiaoj@benrongfax.com>
2020-11-17 17:25:44 +0800

    init

---
+++ precache-manifest.3d3ad9eabedd09bc6182360814b19426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640ce4ea55f79ffd9f3715e2bda8f746",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad07a5da6e0cf696a1ff",
+    "url": "/static/js/2.fe9cbcbc.chunk.js"
+  },
+  {
+    "revision": "45c7e265f14148fc1686dfd367a2dccb",
+    "url": "/static/js/2.fe9cbcbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10207d6dc6449709129e",
+    "url": "/static/js/main.4ae15084.chunk.js"
+  },
+  {
+    "revision": "98f8597c37883c9d2ebc",
+    "url": "/static/js/runtime-main.e99610cc.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  }
+]);
