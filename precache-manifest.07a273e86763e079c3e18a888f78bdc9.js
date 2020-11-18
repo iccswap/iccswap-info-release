d1bb882da576cdc99cbb4f264adfed334eac1f47
xiaojie <xiaoj@benrongfax.com>
2020-11-18 14:43:10 +0800

    正式版上线

---
+++ precache-manifest.07a273e86763e079c3e18a888f78bdc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf749356e73a62612b4e23782e095d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4da44816b719f9285b4a",
+    "url": "/static/js/2.acce6ee0.chunk.js"
+  },
+  {
+    "revision": "45c7e265f14148fc1686dfd367a2dccb",
+    "url": "/static/js/2.acce6ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4659178ee7d87ff1a0",
+    "url": "/static/js/main.b712686d.chunk.js"
+  },
+  {
+    "revision": "98f8597c37883c9d2ebc",
+    "url": "/static/js/runtime-main.e99610cc.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  }
+]);
